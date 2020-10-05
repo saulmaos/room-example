eade4c1778443c1c817bca22f4f82985fdd5cf03
Saul <saul.maos@gmail.com>
2020-10-05 05:20:28 -0500

    part 2 of impl of Room

--- app/src/main/java/com/recodigo/howtouseroom/BooksDao.kt
+++ app/src/main/java/com/recodigo/howtouseroom/BooksDao.kt
@@ -1,8 +1,6 @@
 package com.recodigo.howtouseroom
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 
 /**
  * Created by SAUL on 03/10/2020.
@@ -15,4 +13,16 @@ interface BooksDao {
 
     @Query("SELECT * FROM books")
     fun getAllBooks(): List<BookEntity>
+
+    @Update
+    fun update(book: BookEntity)
+
+    @Delete
+    fun delete(book: BookEntity)
+
+    @Query("SELECT * FROM books WHERE author = :author")
+    fun getBooksByAuthor(author: String): List<BookEntity>
+
+    @Query("SELECT title FROM books")
+    fun getAllTitles(): List<String>
 }
--- app/src/main/java/com/recodigo/howtouseroom/MainActivity.kt
+++ app/src/main/java/com/recodigo/howtouseroom/MainActivity.kt
@@ -2,6 +2,8 @@ package com.recodigo.howtouseroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.room.Room
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,6 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var database: AppDatabase
+    private val books: ArrayList<BookEntity> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,29 +19,85 @@ class MainActivity : AppCompatActivity() {
 
 
         database = Room.databaseBuilder(
-            application, AppDatabase::class.java, AppDatabase.DATABASE_NAME)
+            application, AppDatabase::class.java, AppDatabase.DATABASE_NAME
+        )
             .allowMainThreadQueries()
             .build()
-        saveBooks()
 
         btnBooks.setOnClickListener {
             val books = database.booksDao.getAllBooks()
+            tvBooks.text = "" // This will reset the text of TextView
             books.forEach { book ->
                 tvBooks.append("${book.id}, ${book.title}, ${book.author}, ${book.pubDate}\n")
             }
+            this.books.clear()
+            this.books.addAll(books)
         }
     }
 
     private fun saveBooks() {
-        val book1 = BookEntity(title = "Mobby Dick", author = "Herman Melville", pubDate = "1851")
-        val book2 = BookEntity(title = "Mobby Dick 2", author = "Herman Melville", pubDate = "1851")
-        val book3 = BookEntity(title = "Mobby Dick 3", author = "Herman Melville", pubDate = "1851")
-        val book4 = BookEntity(title = "Mobby Dick 4", author = "Herman Melville", pubDate = "1851")
-        val book5 = BookEntity(title = "Mobby Dick 5", author = "Herman Melville", pubDate = "1851")
+        val book1 = BookEntity(title = "Moby Dick", author = "Herman Melville", pubDate = "1851")
+        val book2 = BookEntity(title = "Taipi", author = "Herman Melville", pubDate = "1846")
+        val book3 =
+            BookEntity(title = "Don Quixote", author = "Miguel de Cervantes", pubDate = "1605")
+        val book4 =
+            BookEntity(title = "El gran Garsby", author = "F. Scott Fitzgerald", pubDate = "1925")
+        val book5 = BookEntity(title = "War and Peace", author = "Leo Tolstoy", pubDate = "1869")
         database.booksDao.insert(book1)
         database.booksDao.insert(book2)
         database.booksDao.insert(book3)
         database.booksDao.insert(book4)
         database.booksDao.insert(book5)
+    }
+
+    //  This will create the menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_activity_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_insert_all -> saveBooks()
+            R.id.action_update -> updateFirstBook()
+            R.id.action_delete -> deleteFirstBook()
+            R.id.action_delete_all -> deleteAll()
+            R.id.action_books_by_author -> getBooksByAuthor()
+            R.id.action_all_titles -> getAllTitles()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun updateFirstBook() {
+        if (books.size > 0) {
+            books.first().title = "Otro libro"
+            database.booksDao.update(books.first())
+        }
+    }
+
+    private fun deleteFirstBook() {
+        if (books.size > 0)
+            database.booksDao.delete(books.first())
+    }
+
+    // This method is provided by Room
+    private fun deleteAll() {
+        database.clearAllTables()
+    }
+
+    private fun getBooksByAuthor() {
+        val books = database.booksDao.getBooksByAuthor("Herman Melville")
+        tvBooks.text = ""
+        books.forEach { book ->
+            tvBooks.append("${book.id}, ${book.title}, ${book.author}, ${book.pubDate}\n")
+        }
+    }
+
+    private fun getAllTitles() {
+        val titles = database.booksDao.getAllTitles()
+        tvBooks.text = ""
+        titles.forEach { title ->
+            tvBooks.append("$title\n")
+        }
     }
 }
